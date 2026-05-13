817c0d27f1b0de2753af0844ade644caf1e9a0f5
wataru0531 <obito0531@gmail.com>
2026-05-13 18:17:12 +0900

    filer-uploader.ts

--- slack-clone-api/src/lib/file-uploader.ts
+++ slack-clone-api/src/lib/file-uploader.ts
@@ -10,51 +10,62 @@ import { Request, Response } from 'express';
 // ✅ 画像ファイルをサーバーに保存して、そのURLとフォームデータを返す処理
 // 呼び出し元の処理　account.controller.ts
 // →  const { fileUrl, body } = await upload(req, res, 'account');
+
 export const upload = ( req: Request, res: Response, folderName: string
 ): Promise<{ fileUrl: string | null; body: any }> => {
-  const destination = `uploads/${folderName}`;
+  const destination = `uploads/${folderName}`; // フォルダ名を定義
 
   mkdirSync(destination, { recursive: true }); // 同期的に保存用フォルダを作成
                                               //  recursive: true ... 必要なフォルダを全部自動生成
                                               //  → falseで、uploadsフォルダがなければエラー
 
-  // ⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから
-  // ⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから
-  // ⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから
-
-  // どう保存するかの設定を組む
-  // 👉 multer ... ファイルを受け取るライブラリ
+  // ✅ どう保存するかの設定を組む → ディスク保存用の設定オブジェクトを生成
+  // → この時点では保存していない。
+  // 👉 multer ... Expressでファイルアップロードを扱うためのライブラリ
   // diskStorage() → ファイルをディスク(サーバーのフォルダ)に保存する方式
   const storage = multer.diskStorage({ 
-    destination, // このフォルダに保存
-    // ファイル名を生成
+    destination, // このフォルダに保存。uploads/account に入れる。
+    // ファイル名をを設定
     filename: (_req: Request, file: Express.Multer.File, cb: any) => {
-      // ファイル名をランダム化 =  現在時刻をミリ秒 + - Math.round((0 〜 1) * 10億)
+      // ファイル名をランダム化 =  現在時刻をミリ秒に変換 + - Math.round((0 〜 1) * 10億)
+      // → 元ファイル名を使うと、同じ名前が来るので、新しいファイルが古いファイルを上書きしてしまう。
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); // 四捨五入
-      // 拡張子を保持
+      // 例: 1715500000000-483726120
+
+      // 拡張子を取得
+      // file.originalname → 元ファイル名が入る
       const ext = path.extname(file.originalname);
+
+      // cb → コールバック
+      // この関数で、multerにファイル名を通知する
       cb(null, uniqueSuffix + ext);
     },
   });
 
+  // ✅ アップロード処理オブジェクトを作成
   const upload = multer({
-    storage, // 保存方法
+    storage, // 保存方法(保存先、ファイル名)
     limits: {
-      fileSize: 20 * 1024 * 1024, // ファイルのサイズ制限。20MBまで
+      fileSize: 20 * 1024 * 1024, // ファイルのサイズ制限。20MBまで。それを超えたらアップロード拒否
     },
   });
 
   return new Promise((resolve, reject) => {
     // ⭐️ 実際のアップロード処理
-    // リクエストの中から、file という名前のファイルを1つ受け取る
+    // upload.single() → middleware関数を返す
+    // const middleware = upload.single() のような感じ。
+    // そして、middleware(req, res, (error: any) => {}) を実行
     upload.single('file')(req, res, (err: any) => {
       if(err != null) return reject(err); // アップロード失敗時
 
+      // ✅ 現在のサーバーのURLを作る
       // URL生成 http://localhost:3000
+      // req.get("host") → リクエストヘッダーのHostを取得
       const baseURL = req.protocol + '://' + req.get('host');
 
       resolve({
-        // ファイルURL http://localhost:3000/uploads/account/123.png
+        // ファイルURLを生成 http://localhost:3000/uploads/account/123.png
+        // req.file.path → multerのuploadオブジェクトの保存方法(保存先、名前)を参照
         fileUrl: req.file != null ? `${baseURL}/${req.file.path}` : null,
         body: req.body, // 画像以外のデータ nameなど
       });
